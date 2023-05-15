4e89e126410d728fd5def430615858af3e19038b
Mizety <122275812+Mizety@users.noreply.github.com>
2023-05-15 21:38:18 +0530

    update

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-
+Route::get('/welcome', function () {
+    return view('welcome');
+});
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
